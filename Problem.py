92e796eccd3c612bc6290281be2dc8431ca33147
ChelseaW <c.winfrey93@yahoo.com>
2019-01-25 13:47:11 -0600

    Review

---
+++ Problem.py
@@ -0,0 +1,72 @@
+def main():
+    problem1()
+    # problem2()
+
+
+# Create a function that has a loop
+# Prompt for numbers until the user enters q to quit
+# If the user doesn't enter q, ask them to input another number
+# When the user enters q to quit, print the SUM of all numbers entered
+# BONUS:
+#
+# Write your code so that it it doesn't matter if the User enters a lowercase or an uppercase q to quit
+# Add extra validation to check that if the User doesn't enter q what they did enter is actually a number
+
+
+# This code has to sum up the numbers that has been entered into the program
+# def problem1():
+#
+#     collection =[]
+#
+#     userInput = ""
+#     while(userInput != "q" ):
+#
+#         # userInput =int(input("Give me something"))print the sum of these numbers
+#         # userInput2 = int(input("Another number"))
+#         userInput= input("q to quit")
+#         if( userInput == 'add'):
+#              print(add(userInput))
+#     print(collection)
+
+        # if (userInput != userInput2):
+        #  userInput = input("That's It")
+
+        # print("IT Work!!")
+
+# This code supoose to apply any math problem to any numbers from the user
+# def problem2():
+#
+#     num1 = int(input("Enter a number"))
+#     num2 = int(input("Enter a number"))
+#
+#
+# def doTheMath(num1,num2):
+#
+#
+#
+#     if(op =='sum'):
+#         print(sum(num1,num2))
+#     elif(op =='difference'):
+#         print(difference(num1,num2))
+#     elif(op =='product'):
+#         print(product(num1, num2))
+#     elif(op =='quotient'):
+#         print(quotient(num1,num2))
+
+    # myDictionary = {
+    #
+    #     'sum': 'num1','num2'
+    #     'difference': 'num1','num2'
+    #     'product': 'num1','num2'
+    #     'quotient': 'num1','num2'}
+    #
+    # return [myDictionary]
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
